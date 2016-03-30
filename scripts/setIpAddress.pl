284d92846dd118583badcaa42f07803ccf6cc26f
JValck <JValck@users.noreply.github.com>
2016-03-30 17:03:23 +0200

    Set static IP Unix systems

--- scripts/setIpAddress.pl
+++ scripts/setIpAddress.pl
@@ -1,9 +1,19 @@
 #!/usr/bin/perl
 
-#get all ip-addresses from subnet
-system("sudo arp-scan 192.168.2.0/24");
-if($? == -1){
-	print "Trying to install arp-scan...\n";
-	system("sudo dnf -y install arp-scan");
-	system("sudo arp-scan 192.168.2.0/24");
+$ipSetted = 0;
+#get all ip-addresses from 192.168.2.50 to 192.168.2.60
+for($idx = 50; $idx < 61; $idx++){
+	$out = qx(ping -c 1 192.168.2.$idx);
+
+	if($out =~ /Unreachable/){
+		#IP does not exist, set this ip
+		print "Found valid IP\n";
+		qx(sudo ip addr add 192.168.2.$idx dev enp2s0);
+		$ipSetted = 1;
+		last;
+	}
+}
+
+if($ipSetted == 0){
+	print "ERROR: Could not set an IP. All the IPs are occupied, try setting an IP manually";
 }
